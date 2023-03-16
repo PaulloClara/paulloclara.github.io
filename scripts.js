bf1d1f71b4eb8a4cdfd913ae0747d0e95c374d7e
PaulloClara <paulloclara@gmail.com>
2023-03-16 19:17:00 -0300

    feat: adiciona som de fundo

--- scripts.js
+++ scripts.js
@@ -1,40 +1,82 @@
 function runThemeScript() {
-  const bodyPageEl = document.querySelector("body");
-  const switchThemeButtonEl = document.querySelector("#pc-switch-theme");
+  try {
+    const bodyPageEl = document.querySelector("body");
+    const switchThemeButtonEl = document.querySelector("#pc-switch-theme");
 
-  switchThemeButtonEl.onclick = () => {
-    const currentTheme = bodyPageEl.dataset.theme === "night" ? "day" : "night";
-    bodyPageEl.dataset.theme = currentTheme;
-    localStorage.setItem("THEME", currentTheme);
-  };
+    switchThemeButtonEl.onclick = () => {
+      const currentTheme =
+        bodyPageEl.dataset.theme === "night" ? "day" : "night";
+      bodyPageEl.dataset.theme = currentTheme;
+      localStorage.setItem("THEME", currentTheme);
+      switchBackgroundSoundByTheme(currentTheme);
+    };
 
-  const savedTheme = localStorage.getItem("THEME");
-  if (savedTheme) bodyPageEl.dataset.theme = savedTheme;
+    const savedTheme = localStorage.getItem("THEME");
+    if (savedTheme) bodyPageEl.dataset.theme = savedTheme;
+    else if (window.matchMedia?.("(prefers-color-scheme: dark)").matches) {
+      bodyPageEl.dataset.theme = "night";
+    }
+
+    switchBackgroundSoundByTheme(bodyPageEl.dataset.theme);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function runBannerTypingAnimationScript() {
-  new Typed("#pc-banner-typing-animation", {
-    strings: ["Paulo", "Frontender", "Criativo", "Entusiasta", "Versátil"],
-    loop: true,
-    typeSpeed: 120,
-    backSpeed: 100,
-    backDelay: 1200,
-  });
+  try {
+    new Typed("#pc-banner-typing-animation", {
+      strings: ["Paulo", "Frontender", "Criativo", "Entusiasta", "Versátil"],
+      loop: true,
+      typeSpeed: 120,
+      backSpeed: 100,
+      backDelay: 1200,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function runScrollSmoothScript() {
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (event) {
-      event.preventDefault();
+  try {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener("click", function (event) {
+        event.preventDefault();
 
-      const target = document.querySelector(this.getAttribute("href"));
-      if (!target) return;
+        const target = document.querySelector(this.getAttribute("href"));
+        if (!target) return;
 
-      target.scrollIntoView({ behavior: "smooth" });
+        target.scrollIntoView({ behavior: "smooth" });
+      });
     });
-  });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function runBackgroundSoundScript() {
+  try {
+    const backgroundSoundEl = document.querySelector("#pc-background-sound");
+    backgroundSoundEl.volume = 0.05;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function switchBackgroundSoundByTheme(theme) {
+  try {
+    const backgroundSoundEl = document.querySelector("#pc-background-sound");
+    backgroundSoundEl.pause();
+    const currentSound = theme === "day" ? "hollow-knight.mp3" : "radiance.mp3";
+    backgroundSoundEl.getElementsByTagName("source")[0].src = currentSound;
+    backgroundSoundEl.load();
+    backgroundSoundEl.play();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 runThemeScript();
 runScrollSmoothScript();
+runBackgroundSoundScript();
 runBannerTypingAnimationScript();
