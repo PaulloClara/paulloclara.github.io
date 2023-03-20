af68466f4ca4f0167f754e3d57f5e31aaba16665
PaulloClara <paulloclara@gmail.com>
2023-03-20 08:37:13 -0300

    feat: inicia animacao de skills apenas quando visivel

--- scripts.js
+++ scripts.js
@@ -84,7 +84,28 @@ function switchBackgroundSoundByTheme(theme) {
   }
 }
 
+function runSkillsAnimationScript() {
+  const skillsSectionEl = document.querySelectorAll("#skills");
+
+  const observer = new IntersectionObserver(
+    function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { rootMargin: "0px", threshold: 0.2 }
+  );
+
+  skillsSectionEl.forEach((element) => {
+    observer.observe(element);
+  });
+}
+
 runThemeScript();
 runScrollSmoothScript();
 runBackgroundSoundScript();
+runSkillsAnimationScript();
 runBannerTypingAnimationScript();
